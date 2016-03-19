0f4e239026898a01810fba32477de385b283984c
Zachary Best <zacharybest87@gmail.com>
2016-03-19 08:30:54 -0400

    User signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#hello'
   get 'signup' => 'users#new'
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
