adf2b3923a88f1658d3776c5ba7fb0379cea8f13
Zachary Best <zacharybest87@gmail.com>
2016-04-03 02:15:26 -0400

    finished servicearea filter by resource

--- app/models/servicearea.rb
+++ app/models/servicearea.rb
@@ -7,6 +7,6 @@ class Servicearea < ActiveRecord::Base
   def get_workorders()
     temp = Workorder.within(self.radius, :origin => [self.lat, self.lng])
     temp = temp.where("distance_value <= ?", self.move_distance)
-    #temp = temp.resources.where("mask <= ?", self.resources.first.mask)
+    temp.joins(:resources).where("mask <= ?", self.resources.first.mask)
   end
 end
