39cb69988cb12b43deb984827260c5d4061c3b8f
Zachary Best <zacharybest87@gmail.com>
2016-04-02 21:51:37 -0400

    removed unneeded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-
-  get 'serviceareas/new'
-
-  get 'serviceareas/create'
-
-  get 'workorders/new'
-
-  get 'workorders/create'
-
-  get 'homepage/index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
     
-  root 'homepage#index'
+  root 'homepage#home'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
