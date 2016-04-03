3a7fb1ad831f1c76bfc2840d48c8b509a8a89e71
Zachary Best <zacharybest87@gmail.com>
2016-04-03 02:37:36 -0400

    servearea now converts miles to meters

--- app/models/servicearea.rb
+++ app/models/servicearea.rb
@@ -2,6 +2,7 @@ class Servicearea < ActiveRecord::Base
   belongs_to :user
   has_many :resources
   acts_as_mappable :auto_geocode=>{:field=>:address, :error_message=>'Could not geocode address'}
+  before_save :convert_to_meters
   
     
   def get_workorders()
@@ -9,4 +10,10 @@ class Servicearea < ActiveRecord::Base
     temp = temp.where("distance_value <= ?", self.move_distance)
     temp.joins(:resources).where("mask <= ?", self.resources.first.mask)
   end
+  
+  private
+  
+    def convert_to_meters()
+      self.move_distance = (move_distance * 1609.34).ceil
+    end
 end
